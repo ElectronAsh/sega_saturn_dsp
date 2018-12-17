81501afb10af3bdb192343f3967aa5789ba60579
Frédéric Mahé <frederic.mahe@cirad.fr>
2018-12-17 10:37:09 +0100

    Reformat
    Hi, I find it easier to read as such. Just a suggestion. Awesome work on the MiSTer!

--- verilate.sh
+++ verilate.sh
@@ -1,1 +1,13 @@
-verilator  -Mdir out -Wno-lint -Wno-COMBDLY -Wno-INITIALDLY -Wno-UNOPTFLAT --cc saturn_dsp.sv --top-module saturn_dsp --exe sim_main.cpp vga_out.cpp EasyBMP.cpp
+#!/bin/sh
+
+verilator \
+    -Mdir out \
+    -Wno-lint \
+    -Wno-COMBDLY \
+    -Wno-INITIALDLY \
+    -Wno-UNOPTFLAT \
+    --cc saturn_dsp.sv \
+    --top-module saturn_dsp \
+    --exe sim_main.cpp vga_out.cpp EasyBMP.cpp
+
+exit 0
